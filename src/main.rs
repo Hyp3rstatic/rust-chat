c1e74e445faf13346a2b1b21b7ef10ec310369cc
kman <kman@kmans-MBP.lan1>
2025-04-09 23:35:25 -0700

    Added rudementary TCP and FTP functionality

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,54 @@
 use std::net::{TcpStream, TcpListener};
 use std::io:: {Read, Write, stdin, stdout};
-//use std::time::Instant;
 use suppaftp::FtpStream;
-//use ftp::{SslContext, SslMethod};
+use unftp_sbe_fs::ServerExt;
 
-//impl user ids that are updated in one handler
-//impl keepalive protocol
-//impl ftp
+async fn serve_ftp(addr_port: String) {
+  let ftp_home = std::env::temp_dir();
+  let server = libunftp::Server::with_fs(ftp_home)
+      .greeting("Welcome to my FTP server")
+      .passive_ports(50000..65535)
+      .build()
+      .unwrap();
+  server.listen(addr_port).await;
+}
 
-/*#[derive(Debug, Clone, Copy)]
-struct conn_id {
-  id: u32,
-  address: String,
-  is_connected: bool,
-}*/
+fn connect_ftp(addr_port: String) {
+  println!("Trying to connect via ftp");
+  let mut ftp_stream = FtpStream::connect(addr_port).unwrap();
+  let _ = ftp_stream.login("username", "password").unwrap();
+  println!("Current directory: {}", ftp_stream.pwd().unwrap());
+  let mut input = String::new();
+  loop {
+    stdin().read_line(&mut input).unwrap();
+    if input == "exit\n" {
+      println!("exiting FTP");
+      break;
+    }
+    stdout().flush().unwrap();
+    input = String::new();
+  }
+}
 
 fn connect(address: &str, port: &u16) -> Result<(), String> {
   let addr_port: String = format!("{}:{}", address, port);
   let mut stream: TcpStream = TcpStream::connect(addr_port.clone()).map_err(|_| format!("connection to host {}:{} failed", address, port))?;
   let mut input = String::new();
-  //let mut now = Instant::now();
   loop {
     stdin().read_line(&mut input).unwrap();
+    stream.write(input.as_bytes()).expect("failed to write input to server");
     if input == "exit\n" {
-      println!("grapes and stuff");
+      println!("disconnecting");
       break;
     }
     else if input == "ftp\n" {
-      let mut ftp_stream = FtpStream::connect(addr_port.clone()).map_err(|_| format!("(FTP) failed to connect to host {}", addr_port))?;
-      println!("Current directory: {}", ftp_stream.pwd().unwrap());
+      //Stream.write("REQUEST FTP SERVER".as_bytes()).expect("failed to write to server");
+      connect_ftp("127.0.0.1:2121".to_string()); 
     }
     else {
       println!("User input: {}", input);
     }
-    stream.write(input.as_bytes()).expect("failed to write input to server");
+      
     stdout().flush().unwrap();
     input = String::new();
   }
@@ -46,30 +61,22 @@ fn handle_connection(mut stream: TcpStream) {
   println!("Client {}", client_addr);
   stream.write(response).expect("failed to write to client");
   loop {
-    //let now = Instant::now();
-      //if(now.as_seconds() == "5") {
         let mut buffer: [u8; 1024] = [0; 1024];
         stream.read(&mut buffer).expect("failed to read from client");
         let request = String::from_utf8_lossy(&buffer[..]);
         println!("{}", request);
-        if request == "exit\n"{
-          //conn[itsid].address = "";
-          //conn[itsid].is_active = false;
+        if request == "exit"{
+          stream.write("Goodbye!".as_bytes()).expect("failed to write to client");
         }
-        //if request == "" {
-          //break;
-        //}
-      //}
   }
 }
 
-fn start_server(address: &str, port: &u16/*, conns: [conn_id] */) {
+fn start_server(address: &str, port: &u16) {
   let listener: TcpListener = TcpListener::bind(&format!("{}:{}", address, port)).expect("failed to start server");
   println!("server listening on {}:{}", address, port);
   for stream in listener.incoming() {
     match stream {
       Ok(stream) => {
-
         std::thread::spawn(|| handle_connection(stream));
       }
       Err(e) => {
@@ -79,27 +86,22 @@ fn start_server(address: &str, port: &u16/*, conns: [conn_id] */) {
   }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
   let mut input = String::new();
   stdin().read_line(&mut input).unwrap();
   if input == "0\n" {
-    /*
-    let mut conn_ids: [conn_id; 10] = [conn_id{
-      id: 0, 
-      address: "",
-      is_connected: false,
-    }; 10];
-    let mut i: u32 = 0;
-    for mut entry in conn_ids {
-      entry.id = i;
-      entry.is_connected = false;
-      i += 1;
-    }
-     */
-    start_server("127.0.0.1", &8080/*, conn_ids*/);
+    std::thread::spawn(|| start_server("127.0.0.1", &8080));
+    serve_ftp("127.0.0.1:2121".to_string()).await;
   }
   else if input == "1\n" {
     connect("127.0.0.1", &8080).unwrap()
+  }
+  else if input == "ftp0\n" {
+    serve_ftp("127.0.0.1:2121".to_string()).await;
+  }
+  else if input == "ftp1\n" {
+    connect_ftp("127.0.0.1:2121".to_string());
   }
   else {
     println!("geek");
