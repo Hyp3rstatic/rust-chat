67c773443ed7982d1b1d5672044af2473c7bb6c0
Hyp3rstatic <kazjrosinski@gmail.com>
2025-06-29 19:19:09 -0700

    ready for test impl

--- src/main.rs
+++ src/main.rs
@@ -26,27 +26,16 @@ async fn main() {
 
 #[cfg(test)]
 mod test {
-  use std::io::{BufRead, BufReader};
-  use std::net::{TcpStream, TcpListener};
-  use std::io:: {Read, Write, stdin, stdout};
-  use suppaftp::FtpStream;
-  use unftp_sbe_fs::ServerExt;
   use chat::connect;
   
+//create a server
+//connect to server
+//on connect recieve "OK"
+//disconnect
+//close server
+
   fn testconn () {
     let stream = connect("127.0.0.1", &9000).unwrap();
-    let mut reader = BufReader::new(stream);
-    let mut line = String::new();
-
-    while reader.read_line(&mut line)? > 0 {
-      println!("Received: {}", line);
-      line.clear();
-    }
-
-  testconn();
-
   }
-  //std::thread::spawn(|| start_server("127.0.0.1", &8080));
-  //std::thread::spawn(|| connect("127.0.0.1", &9000));
 
 }
