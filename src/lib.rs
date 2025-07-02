a809583e6b97d353c2bf33b8563b130073193788
Hyp3rstatic <kazjrosinski@gmail.com>
2025-07-01 22:43:35 -0700

    fixed print formatting and added todo comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 use std::net::{TcpStream, TcpListener};
-use std::io:: {Read, Write, stdin, stdout};
+use std::io:: {self, Read, Write, stdin, stdout};
 
+//TODO: See the messages of other clients
 pub fn connect(address: &str, port: &u16) -> Result<(), String> {
   let addr_port: String = format!("{}:{}", address, port);
   let mut stream: TcpStream = TcpStream::connect(addr_port.clone()).map_err(|_| format!("connection to host {} failed",addr_port))?;
   let mut input = String::new();
+  println!(" --- Joined TCP Server --- ");
   loop {
+    print!(":");
+    io::stdout().flush().unwrap();
     stdin().read_line(&mut input).unwrap();
     stream.write(input.as_bytes()).expect("failed to write input to server");
     if input == "exit\n" {
@@ -13,7 +17,7 @@ pub fn connect(address: &str, port: &u16) -> Result<(), String> {
       break;
     }
     else {
-      println!("User input: {}", input);
+      print!("User Sent Message: {}", input);
     }
 
     stdout().flush().unwrap();
@@ -22,6 +26,7 @@ pub fn connect(address: &str, port: &u16) -> Result<(), String> {
   Ok(())
 }
 
+//TODO: add mutex for serverside display -- later client side as well; Display the ip:port of clients
 pub fn handle_connection(mut stream: TcpStream) {
   let response = "welcome client".as_bytes();
   let client_addr = stream.peer_addr().unwrap();
@@ -31,13 +36,14 @@ pub fn handle_connection(mut stream: TcpStream) {
         let mut buffer: [u8; 1024] = [0; 1024];
         stream.read(&mut buffer).expect("failed to read from client");
         let request = String::from_utf8_lossy(&buffer[..]);
-        println!("{}", request);
+        print!("Connection Sent Message: {}", request);
         if request == "exit"{
           stream.write("Goodbye!".as_bytes()).expect("failed to write to client");
         }
   }
 }
 
+//TODO: if the server is running, automatically connect, if not, start the server
 pub fn start_server(address: &str, port: &u16) {
   let listener: TcpListener = TcpListener::bind(&format!("{}:{}", address, port)).expect("failed to start server");
   println!("server listening on {}:{}", address, port);
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
     connect("127.0.0.1", &8080).unwrap()
   }
   else {
-    println!("geek");
+    println!("Invalid Input - Exiting Program");
   }
 }
 
